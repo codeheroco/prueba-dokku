0efa897fe095b61b86e9508f9d496e0bd7767df3
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-03-23 02:08:37 -0430

    getting real ip with heroku workaround

--- controllers/main/index.js
+++ controllers/main/index.js
@@ -4,8 +4,19 @@ var app = module.exports = express();
 app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res){
+
+	var ipAddress;
+	var forwardedIpsStr = req.header('x-forwarded-for');
+	if (forwardedIpsStr) {
+		var forwardedIps = forwardedIpsStr.split(',');
+		ipAddress = forwardedIps[0];
+	}
+	if (!ipAddress) {
+		ipAddress = req.connection.remoteAddress;
+	}
+
 	res.render('index',{
 		title: 'Prueba de Dokku',
-		ip: req.connection.remoteAddress
+		ip: ipAddress
 	});
 });
